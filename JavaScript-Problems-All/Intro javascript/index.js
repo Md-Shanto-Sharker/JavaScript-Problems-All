bbf74972a5ebc7a18e334f2ca3e1bff30399bf10
unknown <rabbymd55555@gmail.com>
2026-01-22 21:10:17 +0600

    added js basics

--- JavaScript-Problems-All/Intro javascript/index.js
+++ JavaScript-Problems-All/Intro javascript/index.js
@@ -1,1 +1,50 @@
-console.log("Hello World");
+// console.log("Hello World");
+// console.log(`"Hello World"`);
+// console.log(33);
+// console.log(22);
+
+// variable
+var price = 38;
+var year = 2026;
+// change the price value
+price = 40;
+// console.log("price :", price, "year :", year);
+
+// String type
+var name = "our first string";
+// console.log(name);
+var country = "bangladesh";
+// console.log(country);
+
+// boolean type
+var isRich = true;
+// console.log(isRich);
+
+var passed = false;
+// console.log(passed);
+
+// type of>>>>>
+// console.log(typeof(passed));
+// console.log(typeof(country));
+
+var orange = 20;
+var chocolate = 0.5;
+// var applePrice = "20";
+// var applePrice = parseInt("20");
+var applePrice = parseFloat("2.5");
+
+// console.log(orange + applePrice);
+// console.log(typeof applePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(total);
+// console.log(parseFloat(total.toFixed(3)));
+
+const num = 50 / 0;
+console.log(num);
+
+
+const num1 = -50 / 0;
+console.log(num1);
