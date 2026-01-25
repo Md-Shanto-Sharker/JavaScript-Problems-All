1cf555dfd30ff37ec678e6c47e9bdfc001b5ac2a
unknown <rabbymd55555@gmail.com>
2026-01-25 17:42:11 +0600

    added js conditionals some tasks

--- JavaScript-Problems-All/JavaScript_Conditions/multiple.js
+++ JavaScript-Problems-All/JavaScript_Conditions/multiple.js
@@ -51,14 +51,61 @@ const hasCar = false;
 // }
 
 // ------Problem-2----------------
-const balance = 7000;
-const withDraw = 700;
-if (withDraw <= balance) {
-  if (withDraw % 500 === 0) {
-    console.log("WithDraw Successful");
-  } else {
-    console.log("Enter amount multiple of 500");
-  }
+// const balance = 7000;
+// const withDraw = 700;
+// if (withDraw <= balance) {
+//   if (withDraw % 500 === 0) {
+//     console.log("WithDraw Successful");
+//   } else {
+//     console.log("Enter amount multiple of 500");
+//   }
+// } else {
+//   console.log("Insufficient Balance");
+// }
+
+// --------------------------
+// const price = 10000;
+// if (price >= 5000) {
+//   const discount = (price * 10) / 100;
+//   const payAmount = price - discount;
+//   console.log(payAmount);
+// } else if (price > 2500) {
+//   const discount = (price * 5) / 100;
+//   const payAmount = price - discount;
+//   console.log(payAmount);
+// } else {
+//   console.log(price);
+// }
+
+// ---------------------------------------
+// const age = 51;
+// const price = 500;
+// if (age <= 12) {
+//   console.log("You can eat for free");
+// } else if (age >= 60) {
+//   const discount = (price * 50) / 100;
+//   const payAmount = price - discount;
+//   console.log(payAmount);
+// } else if (age >= 50) {
+//   console.log("get 25% discount");
+// } else if (age >= 40) {
+//   console.log("get 30% discount");
+// } else {
+//   console.log(price);
+// }
+
+// ---------------------------------
+const money = 500;
+if (money > 300) {
+  console.log("bro you are rich");
 } else {
-  console.log("Insufficient Balance");
+  if (money > 100) {
+    console.log("tui gorib o na boro lok o na");
+  } else {
+    if (money > 0) {
+      console.log("dosto kola kha.calcium bara");
+    } else {
+      console.log("tui amer bondu na");
+    }
+  }
 }
--- JavaScript-Problems-All/JavaScript_Conditions/ternary.js
+++ JavaScript-Problems-All/JavaScript_Conditions/ternary.js
@@ -1,8 +1,8 @@
 let isMatured = false;
 let isInterested = false;
 
-const isMarried = isMatured ? "biya korba" : "biya korba nah";
-console.log(isMarried);
+// const isMarried = isMatured ? "biya korba" : "biya korba nah";
+// console.log(isMarried);
 
 // -----------------------------------------
 // const isMarried = isMatured ? true : isInterested ? true : false;
@@ -11,3 +11,13 @@ console.log(isMarried);
 // ------------------------------------------
 // let isMarried = isMatured && isInterested ? "korta parba" : "akhn possible nah";
 // console.log(isMarried);
+
+// --------------------------------------
+// const age = 20;
+// age >= 18 ? console.log("Vote dio") : console.log("Ghumai thako");
+
+// ------------------------------------------------
+// let price = 500;
+// const isLeader = false;
+// price = isLeader === true ? (price > 1000 ? price / 2 : 0) : price + 100;
+// console.log(price);
