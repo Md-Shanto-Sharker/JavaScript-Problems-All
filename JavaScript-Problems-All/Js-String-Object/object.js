e07d8061e99f80b1aa35efe80520b201ba6b1832
unknown <rabbymd55555@gmail.com>
2026-02-01 01:10:06 +0600

    added string object reverse concat slice and object and string all task complete

--- JavaScript-Problems-All/Js-String-Object/object.js
+++ JavaScript-Problems-All/Js-String-Object/object.js
@@ -1,4 +1,23 @@
-var person = {};
-person.name = "John";
-person.age = 30;
-console.log(person);
+// var person = {};
+// person.name = "John";
+// person.age = 30;
+// console.log(person);
+
+const bottle = {
+  brand: "apple",
+  price: 45,
+  color: "white",
+  isClean: false,
+};
+
+const subject = {
+  name: "biology",
+  teacher: "rasheda mam",
+  chapters: ["first", "second", "third"],
+  examDate: "30 Feb",
+  exams: {
+    name: "Final Exam",
+    marks: 100,
+  },
+};
+console.log(subject);
