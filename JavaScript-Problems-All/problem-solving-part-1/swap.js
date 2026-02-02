74594d8da5a3d5c57fba876640d52c6c7301857e
unknown <rabbymd55555@gmail.com>
2026-02-02 15:55:24 +0600

    module 18 complete

---
+++ JavaScript-Problems-All/problem-solving-part-1/swap.js
@@ -0,0 +1,24 @@
+// let a = 5;
+// let b = 7;
+// console.log(a, b);
+// const temp = a;
+// a = b;
+// b = temp;
+// // b = a;
+// console.log(a, b);
+
+// -------------------------------
+
+// let x = 5;
+// let y = 7;
+// console.log(x, y);
+// [x, y] = [y, x];
+// console.log(x, y);
+
+
+
+for (var i = 0; i < 5; i++) {
+ 	 setTimeout(function () {
+ 	console.log(i);
+  }, 100);
+}
