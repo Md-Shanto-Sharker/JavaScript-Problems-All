47eaa000cb8951902e8213ba42fcdf003b5996e5
unknown <rabbymd55555@gmail.com>
2026-02-02 19:53:37 +0600

    module 19 first 3 problem complete

---
+++ JavaScript-Problems-All/problem-solving-part-2/min.js
@@ -0,0 +1,6 @@
+const prices = [2000,16000,50000,100000,12000,30000,35000];
+function getMin(numbers){
+    console.log(numbers);
+}
+const cheap = getMin(prices);
+console.log('cheapest one is :gi');
