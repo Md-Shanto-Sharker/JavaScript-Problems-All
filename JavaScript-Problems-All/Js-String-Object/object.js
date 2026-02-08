3e306ee904fdc18ae8d2afd9fe3c40bd70a152a8
unknown <rabbymd55555@gmail.com>
2026-02-08 20:49:00 +0600

    complete bonus module

--- JavaScript-Problems-All/Js-String-Object/object.js
+++ JavaScript-Problems-All/Js-String-Object/object.js
@@ -3,21 +3,56 @@
 // person.age = 30;
 // console.log(person);
 
-const bottle = {
-  brand: "apple",
-  price: 45,
-  color: "white",
-  isClean: false,
-};
+// const bottle = {
+//   brand: "apple",
+//   price: 45,
+//   color: "white",
+//   isClean: false,
+// };
 
-const subject = {
-  name: "biology",
-  teacher: "rasheda mam",
-  chapters: ["first", "second", "third"],
-  examDate: "30 Feb",
-  exams: {
-    name: "Final Exam",
-    marks: 100,
+// const subject = {
+//   name: "biology",
+//   teacher: "rasheda mam",
+//   chapters: ["first", "second", "third"],
+//   examDate: "30 Feb",
+//   exams: {
+//     name: "Final Exam",
+//     marks: 100,
+//   },
+// };
+// console.log(subject);
+
+const nayok = {
+  name: "Sakib Khan",
+  id: 121,
+  address: "movie cinema",
+  isSingle: true,
+  friends: ["Apu", "Raaz", "Salman", "aamir"],
+  movies: [
+    {
+      name: "no.1",
+      year: 2015,
+    },
+    {
+      name: "king khan",
+      year: 2018,
+    },
+  ],
+  act: function () {
+    console.log("Hi this is a function");
+  },
+  car: {
+    brand: "tesla",
+    price: 5000000,
+    made: 2025,
+    manufacturer: {
+      name: "tesla",
+      ceo: "Elon Mask",
+      country: "USA",
+    },
   },
 };
-console.log(subject);
+
+// nayok.act();
+console.log(nayok.act);
+// console.log(student.car.manufacturer);
