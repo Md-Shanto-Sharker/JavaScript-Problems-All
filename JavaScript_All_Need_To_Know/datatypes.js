b07befca1d59692d239a3277211b5fa5febfc714
unknown <rabbymd55555@gmail.com>
2026-02-09 10:59:16 +0600

    js extaintion and variable and nameing conventions and defferent types of datatypes

---
+++ JavaScript_All_Need_To_Know/datatypes.js
@@ -0,0 +1,40 @@
+/**
+ primitive datatypes are =>
+     1.Number,
+     2.String,
+     3.Boolean,
+     4.null,
+     5.undefined
+ */
+
+/**
+ non primitive datatypes are=>
+     1.Object,
+     2.Array,
+     3.Function
+ *
+ */
+
+//*****Numbers */*********** */
+
+// var n = 1451;
+// var f = 3.14;
+// var nn = Number("45.5");
+// console.log(nn);
+// console.log(Number.parseInt(nn));
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(1 / 0); //Infinity
+// console.log("abc" * 10); //Nan
+
+//*****String */*********** */
+
+var str = "String";
+var str2 = "String";
+var str3 = `String`;
+var str4 = String("sdffsdfsd");
+var str5 = String(10);
+var str6 = String(3.1416);
+console.log(typeof str4, str4);
+console.log(typeof str5, str5);
+console.log(typeof str6, str6);
